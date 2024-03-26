b5241e1ea3d85397cb0d4c9cfdf5293f1de49e27
unknown <AbeeraKhalid>
2024-03-26 07:03:36 +0500

    edit console.log of else

--- index.ts
+++ index.ts
@@ -6,7 +6,7 @@ const answer = await inquirer.prompt ([
   { message: "Enter first number", type: "number", name: "firstnumber" },
   { message: "Enter second number", type: "number", name: "secondnumber" },
   {
-    message: "select one of the operator to perform operation",
+    message: "Select one of the operator to perform operation",
     type:"list",
     name:"operator",
     choices:["Addition","Subtraction","Division","Multiplication"],}
